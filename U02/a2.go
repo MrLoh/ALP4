6975a274f2f7a18d1e5fc74d5b59c0cd84505363
MrLoh <flojoto@hotmail.com>
2015-04-28 11:44:11 +0200

    finished U2 a2 programming

--- U02/a2.go
+++ U02/a2.go
@@ -1,16 +1,80 @@
-package "main"
+package main
 
-type List iterface {
-	New() List
+import "fmt"
+
+type List interface {
+	Insert(int) List
+	InsertAt(int, int) List
+	DeleteAt(int) List
 }
 
 type node struct {
-	item Int
-	prev* node
-	next* node
+	item       int
+	prev, next *node
 }
 
 func New() List {
 	return &node{}
 }
 
+func (pos *node) String() string {
+	out := "|"
+	for pos.next != nil {
+		pos = pos.next
+		out += fmt.Sprintf("%v|", pos.item)
+	}
+	return out
+}
+
+func (root *node) Insert(el int) List {
+	pos := root
+	for pos.next != nil {
+		pos = pos.next
+	}
+	newNode := node{item: el, prev: pos, next: nil}
+	pos.next = &newNode
+	return root //for method chaining
+}
+
+func (root *node) InsertAt(el int, n int) List {
+	pos := root
+	for i := 0; i < n; i++ {
+		pos = pos.next
+	}
+	if pos.next != nil {
+		newNode := node{item: el, prev: pos, next: pos.next}
+		pos.next.prev = &newNode
+		pos.next = &newNode
+	} else {
+		newNode := node{item: el, prev: pos, next: nil}
+		pos.next = &newNode
+	}
+	return root //for method chaining
+}
+
+func (root *node) DeleteAt(n int) List {
+	pos := root
+	for i := 0; i < n; i++ {
+		pos = pos.next
+	}
+	if pos.next != nil {
+		if pos.next.next != nil {
+			pos.next = pos.next.next
+			pos.next.next.prev = pos
+		} else {
+			pos.next = nil
+		}
+	}
+	return root //for method chaining
+}
+
+func main() {
+	list := New().Insert(1).Insert(9).Insert(5)
+	fmt.Println(list)
+
+	list.DeleteAt(2)
+	fmt.Println(list)
+
+	list.InsertAt(0, 2)
+	fmt.Println(list)
+}
